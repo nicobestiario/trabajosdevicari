7e8a58edcb5ec1787e4b7b39f87c521df4da9fa5
nicobestiario <nicolasdevicari@gmail>
2022-11-30 18:32:38 -0300

    t9

---
+++ Trabajo 9 Nicolás De Vicari/JS/funcion.js
@@ -0,0 +1,13 @@
+let pais = "España" ;
+let continente = "Europa" ;
+let antiguedad = "2021" ;
+let pais_y_continente = pais + " " + continente;
+
+
+pais= "Mexico";
+
+continente= "Latinoamerica";
+
+console.log(pais, continente, antiguedad);
+
+alert("¡Esto es una alerta!");
