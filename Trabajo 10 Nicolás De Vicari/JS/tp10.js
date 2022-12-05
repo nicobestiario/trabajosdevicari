5981b3c5a93bde073627664c9cf16607866e3293
nicobestiario <nicolasdevicari@gmail>
2022-12-04 22:10:28 -0300

    tp10

---
+++ Trabajo 10 Nicolás De Vicari/JS/tp10.js
@@ -0,0 +1,55 @@
+
+let despedida = "Nos vemos allá";
+let nombre = "Programación" + " Fácil.";
+let saludo = "Les doy la bienvenida a";
+
+let fraseCompleta = saludo + " " + nombre + " " + despedida;
+console.log(fraseCompleta);
+
+let frase = "JavaScript" + " " + "es un lenguaje" + " " + "esencial para crear" + " " + "sitios web poderosos.";
+console.log(frase);
+
+let numeroString1 = "10";
+let pesos = "pesos"
+let numeroString2 = "20";
+let dolares = "dólares"
+let numeroString3 = "30";
+let euros = "euros"
+
+let concatenaNumeros = numeroString1 + pesos + " " + numeroString2 + dolares + " " + numeroString3 + euros;
+console.log(concatenaNumeros);
+
+var age = prompt("Cuál es tu edad:?");
+var name = prompt("Cuál es tu nombre:?");
+console.warn(age);
+console.warn(name);
+
+let sexo;
+let edad;
+
+sexo = prompt("Mujer u Hombre")
+edad = prompt("Introduce tu edad");
+
+if (sexo = "Hombre")
+if (edad >= 18)
+{
+alert("Puedes entrar");
+} 
+else 
+{
+if (sexo = "Mujer")
+if (edad >= 20)    
+alert("Puedes entrar");
+}
+else
+{
+    if (sexo = "Hombre")
+    if (edad <= 18)
+        alert("No puedes entrar");
+}
+{
+    if (sexo = "Mujer")
+    if (edad <= 20)
+        alert("No puedes entrar");
+}
+
