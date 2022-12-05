56647fbcadccb327428f05e4488e466ff12a8281
nicobestiario <nicolasdevicari@gmail>
2022-12-04 22:39:27 -0300

    tp11

---
+++ Trabajo 11 Nicolás De Vicari/JS/tp11.js
@@ -0,0 +1,33 @@
+let diaSemana = prompt("¿Qué día es hoy?");
+switch (diaSemana) {
+case "lunes":
+alert("Caminá 10 minutos");
+ break;
+case "martes":
+alert("Caminá 20 minutos");
+ break;
+case "miércoles":
+alert("Caminá 30 minutos");
+ break;
+case "jueves":
+alert("Caminá 40 minutos");
+ break;
+ case "viernes":
+alert("Caminá 30 minutos");
+ break;
+case "sábado":
+alert("Caminá 20 minutos");
+ break;
+case "domingo":
+alert("Caminá 10 minutos");
+ break;
+ default:
+alert("Escribe el día de la semana en minúsculas.");
+}
+
+for(i=20;i<=70;i++)
+{
+    document.write("El número es: " + i + "<br>");
+    }
+
+
